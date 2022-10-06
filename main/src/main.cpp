b5a5575810bfb59bb40478e153f961c1b93af7d3
Mauro Levra <mauro@nazardo.it>
2022-10-06 15:43:31 +0200

    Timer.1 - Using a timer synchronously

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#include <asio.hpp>
+
 int main(int argc, char** argv) {
     std::cout << "Learning Asio " << APP_VERSION << std::endl;
+
+    asio::io_context io;
+
+    // Create a timer to expire 5 seconds from now
+    asio::steady_timer t(io, asio::chrono::seconds(5));
+    t.wait(); // blocking wait on the timer
+
     return 0;
 }
